3b62bc9ae06bd181d6c754fc1565dae5b9b6ae7d
Dharr223 <daniel.s.harris223@gmail.com>
2014-10-05 18:55:42 -0400

    Server Structure
    Creates all relevant files to match the relative paths of the files we
    currently have on the server. Also copied the contents of the files at
    the current moment.

---
+++ cgi-bin/hello.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+
+# Gabe Issa - CSC 210
+# put in public_html/cgi-bin/ and set the proper execute permissions
+
+import cgi
+import datetime
+
+# to facilitate debugging
+import cgitb
+cgitb.enable()
+
+import sqlite3
+
+form = cgi.FieldStorage()
+
+name = form['my_name'].value
+age = form['my_age'].value
+
+conn = sqlite3.connect('people.db')
+c = conn.cursor()
+
+try:
+	c.execute('insert into users values(?,?):', (name, int(age)))
+	conn.commit()
+except sqlite3.IntegrityError:
+	pass
+
+print "Content-type: text/html"
+# don't forget the extra newline!
+print
+
+print "<html>"
+print "<head><title>My webpage</title></head>"
+print "<body>"
+print "<h1>Hello world!!!!!!!!</h1>"
+print "<h2>The time is: " + str(datetime.datetime.now()) + "</h2>"
+print "<h2>Your name is: " + form['my_name'].value + "</h2>"
+print "<h2>Your age is: " + form['my_age'].value + "</h2>"
+print "<pre>"
+
+for row in c.execute('select * from users where age > 10 order by age'):
+	print 'Name:', row[0], '|Age:, row[1]
+
+print "</pre>"
+print "</body>"
+print "</html>"
+

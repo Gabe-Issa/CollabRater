3b9a601ce7e6c74c0c3193af0b9a0ebda822f144
Dharr223 <daniel.s.harris223@gmail.com>
2014-10-16 17:51:32 -0400

    Logout
    Adds logout materials (not finished)

---
+++ cgi-bin/logout.py
@@ -0,0 +1,97 @@
+#!/usr/bin/python
+
+
+
+# Daniel Harris, Gabe Issa, Ethan Lipkind
+
+
+import cgi
+
+import datetime
+
+
+
+# to facilitate debugging
+
+import cgitb
+
+cgitb.enable()
+
+import sqlite3
+
+import os
+
+import uuid
+
+form = cgi.FieldStorage()
+
+usrname = form['user_name'].value
+
+password = form['pass_word'].value
+
+session_id = str(uuid.uuid4())
+
+conn = sqlite3.connect('accounts.db')
+c = conn.cursor()
+
+try:
+	c.execute('select * from users where username=?;', (usrname,))
+	all_results = c.fetchall()
+
+	if len(all_results) > 0:
+		
+		
+		print "Content-type: text/html"
+
+		# don't forget the extra newline!
+
+		print
+
+
+
+		print "<html>"
+
+		print "<head><title>CollabRater</title></head>"
+
+		print "<body>"
+
+		print "<h1>Error!</h1>"
+
+		print "<h2>Username already exists. <a href = '../register.html'>Please try another</a></h2>"
+
+		print "<h2>Or <a href='../index.html'>Log In</a></h2>"
+
+		print "</body>"
+
+		print "</html>"
+
+	else:
+	
+		c.execute('insert into users values(?,?,null);', (usrname, password))
+		conn.commit()
+
+		print "Content-type: text/html"
+
+		# don't forget the extra newline!
+
+		print
+
+
+
+		print "<html>"
+
+		print "<head><title>CollabRater</title></head>"
+
+		print "<body>"
+
+		print "<h1>Account Successfully Created</h1>"
+
+		print "<h2><a href='../index.html'>Return to Log In Page</a></h2>"
+
+		print "</body>"
+
+		print "</html>"
+
+except sqlite3.IntegrityError:
+    pass
+

ee40cdd60bb546d43320b6ae387ffeba0154d2dd
Gabe-Issa <Gabe@dhcp-10-5-17-169.wireless.rochester.edu>
2014-11-11 21:42:38 -0500

    tried everything...nothing is working
    can revert back to old stuff if you want but professors code doesn’t
    even work. I think the problem is we have a post method and ajax on the
    same thing and it just will never work. keep trying, but it might not
    be able to be done. I found so many errors in the initial stuff we had,
    but no matter how much I fix, nothing ever works

--- cgi-bin/namecheck.py
+++ cgi-bin/namecheck.py
@@ -1,4 +1,3 @@
-~/public_html/cgi-bin/namecheck.py
 
 #!/usr/bin/python
 
@@ -10,27 +9,39 @@ cgitb.enable()
 # this python script will check the database to see whether the attempted username already exists.
 
 form = cgi.FieldStorage()
+stuff = form['stuff'].value
+f = open('data.txt', 'a')
+f.write(stuff)
+f.write('\n')
+f.close()
+
+all_contents = open('data.txt').read()
+
+print "Content-type: text/html"
+# don't forget the extra newline!
+print
+print all_contents
 
 # this is what the user typed in
-usrname = form['#username'].value
+#usrname = form['usrname'].value
 
-conn = sqlite3.connect('accounts.db')
-c = conn.cursor()
+#conn = sqlite3.connect('accounts.db')
+#c = conn.cursor()
 
-try:
+#try:
 	#I want to pass username as a parameter from the input form on register.html
-	c.execute('select * from users where username=?;', (usrname,))
-	all_results = c.fetchall()
+#	c.execute('select * from users where username=?', (usrname,))
+#	all_results = c.fetchall()
 	
-	if len(all_results)>0:
-		print "Content-type: application/json"
-		print #extra line
-		#  username already exists, need to print out a red X to the div.
-		print '{"result": "TRUE"}'
-	else:
-		print "Content-type: text/plain"
-		print #extra line
-		# username does not exist yet, need to print out a green checkmark.
-		print '{"result": "FALSE"}'
-except sqlite3.IntegrityError:
-	pass
+#	if len(all_results) > 0:
+#		print "Content-type: application/json"
+#		print #extra line
+#		#  username already exists, need to print out a red X to the div.
+#		print '{"result": "Used Username"}'
+#	else:
+#		print "Content-type: application/json"
+#		print #extra line
+#		# username does not exist yet, need to print out a green checkmark.
+#		print '{"result": "Accepted Username"}'
+#except sqlite3.IntegrityError:
+#	pass

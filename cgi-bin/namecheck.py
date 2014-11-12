1cc71d5b0e16938198a480e93772839f800ee634
Gabe-Issa <Gabe@dhcp-10-5-17-169.wireless.rochester.edu>
2014-11-11 22:13:02 -0500

    finally got it ready to go
    we have some ajax boys

--- cgi-bin/namecheck.py
+++ cgi-bin/namecheck.py
@@ -1,47 +1,29 @@
-
 #!/usr/bin/python
 
-import cgi
-import cgitb
+# Remember to make this executable: chmod a+x week8-lab.py
 import sqlite3
-cgitb.enable()
+import cgi
 
-# this python script will check the database to see whether the attempted username already exists.
+# to facilitate debugging
+import cgitb
+cgitb.enable()
 
 form = cgi.FieldStorage()
 stuff = form['stuff'].value
-f = open('data.txt', 'a')
-f.write(stuff)
-f.write('\n')
-f.close()
 
-all_contents = open('data.txt').read()
+conn = sqlite3.connect('accounts.db')
+c = conn.cursor()
 
-print "Content-type: text/html"
-# don't forget the extra newline!
-print
-print all_contents
-
-# this is what the user typed in
-#usrname = form['usrname'].value
-
-#conn = sqlite3.connect('accounts.db')
-#c = conn.cursor()
-
-#try:
-	#I want to pass username as a parameter from the input form on register.html
-#	c.execute('select * from users where username=?', (usrname,))
-#	all_results = c.fetchall()
-	
-#	if len(all_results) > 0:
-#		print "Content-type: application/json"
-#		print #extra line
-#		#  username already exists, need to print out a red X to the div.
-#		print '{"result": "Used Username"}'
-#	else:
-#		print "Content-type: application/json"
-#		print #extra line
-#		# username does not exist yet, need to print out a green checkmark.
-#		print '{"result": "Accepted Username"}'
-#except sqlite3.IntegrityError:
-#	pass
+try:
+	c.execute('select * from users where username=?', (stuff,))
+	all_results = c.fetchall()
+	if len(all_results) > 0:
+		print "Content-type: application/json"
+		print
+		print '{"name": "Not Valid"}'
+	else:
+		print "Content-type: application/json"
+		print
+		print '{"name": "Valid"}'
+except sqlite3.IntegrityError:
+	pass
